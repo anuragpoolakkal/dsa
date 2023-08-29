fe240ed7b7f01035064b039fa8693e6eb5dda7ce
anuragrajanp <anuragrajanp@gmail.com>
2023-08-29 09:38:32 +0530

    peak-index-in-a-mountain-array

---
+++ problems/peak-index-in-a-mountain-array.js
@@ -0,0 +1,20 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0, end = arr.length -1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] > arr[mid+1]) {
+                // you are in the dec part of the array. this may be the ans, but look at left.
+                // this is why end != mid -1
+                end = mid;
+            } else {
+                // you are in the asc part of the array
+                start = mid + 1; // bcz mid+1 element > mid element
+            }
+        }
+        //in the end start == end, pointing to the largest number
+        return start;
+    }
+}
